8741ce55017f36aab096a7102bf01c6ca423e8e5
MrBig83 <mjohansson176@gmail.com>
2023-04-19 16:43:15 +0200

    Första stegen av egen post-typ: 'Nyheter' skapad

--- wp-content/plugins/mj-news/mj-news.php
+++ wp-content/plugins/mj-news/mj-news.php
@@ -2,6 +2,7 @@
 
 /*
 Plugin Name: MJ - News
+Description: Ett plugin för att visa nyheter. Pluginet innehåller även en egen post-typ (news), där användaren kan lägga till nyheter efter behov. 
 */
 
 function skriv_nyheter(){
@@ -9,5 +10,21 @@ function skriv_nyheter(){
 }
 
 add_action ('main_news', 'skriv_nyheter');
+
+
+
+function min_posttyp(){
+    register_post_type( 'news' , array(
+        'labels' => array(
+            'name' => 'Nyheter',
+            'singular_name' => 'Nyhet'),
+            'public' => true,
+            'has_archive' => true
+        ));
+    }
+    
+add_action( 'init' , 'min_posttyp' );
+
+
 
 ?>
